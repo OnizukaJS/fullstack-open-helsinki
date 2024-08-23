4aa6830ef5f089ad19724b5092ec3e6a493274f8
Jean-Baptiste <jeanbaptiste.castillo@cledara.com>
2024-08-23 23:11:19 +0200

    exercise part 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
 
   const addGoodFeedback = () => {
     setGood(good + 1)
@@ -16,6 +17,17 @@ const App = () => {
 
   const addBadFeedback = () => {
     setBad(bad + 1)
+  }
+
+  const calculateAverage = (good, bad) => {
+    if (!good && !bad) return null
+    const totalScore = (good * 1) + (bad * -1)
+    return totalScore / all
+  }
+
+  const calculatePositive = (good) => {
+    if (!good) return null
+    return `${(good * 100) / all} %`
   }
 
   return (
@@ -30,6 +42,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {calculateAverage(good, bad)}</div>
+      <div>positive {calculatePositive(good)}</div>
     </div>
   )
 }
